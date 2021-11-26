4f6b69595282e95ed388570a5f0d2e3a3e41cda4
Arindam <ghosharindam195@gmail.com>
2021-11-26 22:38:03 +0530

    26-11-21

---
+++ manipulators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(void)
+{
+    // cout<<"Hello Amogh"<<endl;
+    // cout<<"Amogh"<<ws<<"Shrivastava";
+
+    // cout<<hex<<163<<endl;
+    int n;
+    istringstream("27") >> oct >> n;  //string->stream
+    cout<<n;
+    // cout  << "Parsing \"2A\" as hex gives " << n << '\n';  
+}
