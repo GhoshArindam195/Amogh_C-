354ada35da56b6674aa9599a46ce206fc61c6c09
Arindam <ghosharindam195@gmail.com>
2021-12-01 22:50:46 +0530

    01-12-21

---
+++ STL/vectorFuncs.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(void)
+{
+    // vector<int> vec{1, 2, 3, 4};
+    // vector<int> vec1{10, 11, 12, 13};
+
+    // cout<<vec[2]<<endl;;
+    // cout<<vec.at(2)<<endl;
+    // cout<<vec.back()<<endl;
+    // cout<<vec.front()<<endl;
+
+    // vec.swap(vec1);
+
+    // for(int n : vec)
+    // {
+    //     cout<<n<<" ";
+    // }
+    // cout<<endl;
+    // for(int n : vec1)
+    // {
+    //     cout<<n<<" ";
+    // }
+    // cout<<endl;
+
+    // vec.insert(vec.begin()+1, 100);
+    // for(int n : vec)
+    // {
+    //     cout<<n<<" ";
+    // }
+
+    // vector<string> vec{"Amogh", "Shrivastava"};
+    // vec.insert(vec.begin()+1, 2, "Amogh");
+
+    // for(string s : vec)
+    //     cout<<s<<" ";
+
+
+}
