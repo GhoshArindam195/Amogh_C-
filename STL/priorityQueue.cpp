550e7e712bab544c8ab516a2902918e3450c0c7f
Arindam <ghosharindam195@gmail.com>
2021-12-28 10:30:39 +0530

    28-12-21

---
+++ STL/priorityQueue.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<queue>
+
+ using namespace std;
+
+ int main()
+ {
+    priority_queue<int> maxi;
+
+    maxi.push(1);
+    maxi.push(5);
+    maxi.push(2);
+    maxi.push(4);
+    maxi.push(0);
+
+    int n = maxi.size();
+
+    for(int i=1; i<=n; i++)
+    {
+        cout<<maxi.top()<<" ";
+        maxi.pop();
+    }
+
+    cout<<endl;
+
+    priority_queue<int, vector<int>, greater<int>> mini;
+    mini.push(5);
+    mini.push(1);
+    mini.push(0);
+    mini.push(4);
+    mini.push(3);
+
+    int m = mini.size();
+
+    for(int i=0; i<m; i++)
+    {
+        cout<<mini.top()<<" ";
+        mini.pop();
+    }
+    cout<<endl;
+
+    cout<<"Is queue Empty ?"<<mini.empty();
+ } 
